ebcb4641f2f4aa8e8e30d3f511f1ca2b41fc3b1a
Samuel-Kim787 <joven.rakimkrishna@ue.edu.ph>
2025-07-21 01:55:00 +0000

    Print Hello

---
+++ hello.cs
@@ -0,0 +1,24 @@
+
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("hello");
+    }
+}
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+    }
+}
+
+
+
+
+
+
+
